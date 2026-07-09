135f2209f3387007927f476d6aef2084322e90da
Helder Daiha <helderdaiha@MacBook-Pro-de-Helder.local>
2026-07-08 21:17:15 -0300

    first-step-to-study-agent

---
+++ code/2_langchain.py
@@ -0,0 +1,23 @@
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+api_key = os.getenv("OPENAI_API_KEY")
+
+numero_dias = 7
+numero_criancas = 2
+atividade = "praia"
+
+prompt = f""""
+    Crie um roteiro de viagens, para um período de {numero_dias}, para uma família com {numero_criancas} crianças que busca atividades relacionadas a {atividade}
+"""
+
+modelo = ChatOpenAI(
+    model = "gpt-3.5-turbo",
+    temperature = 0.5,
+    api_key = api_key
+)
+
+resposta = modelo.invoke(prompt)
+print(resposta.content)
