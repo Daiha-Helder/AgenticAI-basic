bb274026d6adbb3e606cebbf26be6e09245faff9
Helder Daiha <helderdaiha@MacBook-Pro-de-Helder.local>
2026-07-11 19:16:03 -0300

    agent-chat

--- code/chains/4_langchain.py
+++ code/chains/4_langchain.py
@@ -1,4 +1,8 @@
 from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.chat_history import InMemoryChatMessageHistory
+from langchain_core.runnables.history import RunnableWithMessageHistory
 from dotenv import load_dotenv
 import os
 
@@ -14,12 +18,43 @@ modelo = ChatOpenAI(
     api_key=api_key
 )
 
+
+prompt_sugestao = ChatPromptTemplate.from_messages(
+    [
+        ("system", "Você é um guia de viagem especializado em destinos brasileiros. Apresente-se como Sr. Passeios"),
+        ("placeholder", "{historico}"),
+        ("human", "{query}")
+    ]
+)
+
+cadeia = prompt_sugestao | modelo | StrOutputParser()
+
+memoria = {}
+sessao = "chat_langchain"
+
+def historico_por_sessao(sessao:str):
+    if sessao not in memoria:
+        memoria[sessao] = InMemoryChatMessageHistory()
+    return memoria[sessao]
+
 lista_perguntas = [
     "Quero visitar um lugar no Brasil, famoso por praias e cultura. Pode sugerir?",
     "Qual a melhor época do ano para ir?"
 ]
 
+cadeia_com_memoria = RunnableWithMessageHistory(
+    runnable=cadeia,
+    get_session_history=historico_por_sessao,
+    input_messages_key="query",
+    history_messages_key="historico"
+)
+
 for pergunta in lista_perguntas:
-    resposta = modelo.invoke(pergunta)
+    resposta = cadeia_com_memoria.invoke(
+        {
+            "query": pergunta
+        },
+        config={"session_id":sessao}
+    )
     print("Usuário: ", pergunta)
-    print("IA: ", resposta.content, "\n")
+    print("IA: ", resposta, "\n")
