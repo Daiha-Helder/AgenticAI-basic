7c44c8f3014357fa293e4f782a91dab9ec03e0ff
Helder Daiha <helderdaiha@MacBook-Pro-de-Helder.local>
2026-07-15 10:42:33 -0300

    exclude-textload

--- code/langchain_basic/rag/2_rag_pdf.py
+++ code/langchain_basic/rag/2_rag_pdf.py
@@ -1,5 +1,5 @@
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
-from langchain_community.document_loaders import TextLoader, PyPDFLoader
+from langchain_community.document_loaders import PyPDFLoader
 from langchain_community.vectorstores import FAISS
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_core.prompts import ChatPromptTemplate
