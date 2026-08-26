26d91fb8b8fc45f38d1ce1d5531f0f87fa4f6e4f
Daiha-Helder <daihahelder@gmail.com>
2026-08-26 17:59:15 -0300

    first-agent-with-tavily

--- code/4_langgraph/2_components.py
+++ code/4_langgraph/2_components.py
@@ -4,6 +4,7 @@ import operator
 
 import google.generativeai as genai
 from langchain_google_genai import ChatGoogleGenerativeAI
+from IPython.display import Image, display
 
 from langchain_core.messages import (
     AnyMessage,
@@ -19,16 +20,20 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
+import warnings
+warnings.filterwarnings(action='ignore')
+
+
 GEMINI_API_KEY = os.getenv('GEMINI_API_KEY')
 TAVILY_API_KEY = os.getenv('TAVILY_API_KEY')
 
 # %% Defininido Tavily tool
-tool = TavilySearchResults(
+tool_instance = TavilySearchResults(
     max_results = 4
 )
 
-print(type(tool))
-print(tool.name)
+# print(type(tool))
+# print(tool.name)
 
 # %% Agente que acrescenta uma mensagem ao histórico
 class AgentState(TypedDict):
@@ -50,7 +55,7 @@ class Agent:
         graph.add_edge("action", "llm")
         graph.set_entry_point("llm")
         self.graph = graph.compile()
-        self.tool = {t.name:t for t in tools}
+        self.tools = {t.name:t for t in tools}
         self.model = model.bind_tools(tools)
 
     def exist_action(self, state: AgentState):
@@ -75,18 +80,56 @@ class Agent:
             print(f"Calling: {t}")
             if not t['name'] in self.tools:
                 print("\n ...bad tool name...")
-                result = "bad tool name, retry"
+                tool_output = "bad tool name, retry"
             else:
-                result = self.tools[t['name'].invoke(t['args'])]
+                tool_output = self.tools[t['name']].invoke(t['args'])
             result.append(ToolMessage(
                 tool_call_id = t['id'],
                 name = t['name'],
-                content=str(result)
+                content=str(tool_output)
             ))
         print("Back to the model")
         return {"messages": result}
 
+# %% Defininido o agente
+prompt = """
+Você é um assistente de pesquisa inteligente. Use o mecanismo de busca para procurar informações. \
+Você tem permissãos para fazer múltiplas chamadas (seja em conjunto ou em sequência). \
+Procure informações apenas quando tiver certeza o que você quer. \
+Se precisar pesquisar alguma informação antes de fazer uma pergunta de acompanhamento, você tem permissão para fazer isso!.  
+"""
 
-            
+model = ChatGoogleGenerativeAI(
+    model='gemini-3.5-flash-lite',
+    temperature = 0
+)
+
+abot = Agent(model, [tool_instance], system=prompt)
+messages = [HumanMessage(
+    content="Como está o tempo em São Paulo hoje?"
+)]
+
+print("Iniciando interação do agente: ")
+final_result_state = None
+
+for s in abot.graph.stream({"messages": messages}):
+    print(s)
+    print("---")
+    final_result_state = s
+
+print("\nResultado Final:")
+if final_result_state and ('llm' in final_result_state) and final_result_state['llm']['messages']:
+    print(final_result_state['llm']['messages'][-1].content)
+else:
+    print("Nenhum resultado final ou resultado inesperado.")
+
+# try:
+#     image_data = abot.graph.get_graph().draw_mermaid_png()
+#     display(Image(data=image_data))
+# except Exception as e:
+#     print(f"Erro ao tentar gerar PNG do Marmaid: {e}")
+#     print("\nCertifique-se de que a sua versão do LangGraph possui o método `.draw_mermaid_png()`")
+#     print("Como alternativa, use `.draw_mermaid()` para obter a string e visualizar externamente.")
+
 
 
